a1b237a1bdcf6604ba21fab92d35af31977c291a
“ted” <ted@n41.com>
2021-04-29 12:46:24 +0900

    Test DB connection

---
+++ src/main/java/com/nouvolution/accinfo/Test.java
@@ -0,0 +1,15 @@
+package com.nouvolution.accinfo;
+
+public class Test {
+	
+	private String ip;
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+}
---
+++ src/main/java/com/nouvolution/testDb2/HomeController.java
@@ -0,0 +1,51 @@
+package com.nouvolution.testDb2;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.nouvolution.accinfo.Test;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		
+		ctx.load("classpath:root-context.xml");
+		ctx.refresh();
+		
+		Test test = (Test)ctx.getBean("test");
+		
+		System.out.println(test.getIp());
+		
+		return "home";
+	}
+	
+}
